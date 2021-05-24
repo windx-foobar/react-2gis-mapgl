7e253b08421c3ea0be1331530161c3b4ffb61e6f
BarabanMotion <bezalkogoln1yCoder@gmail.com>
2021-05-24 15:29:22 +0300

    Добавлен хелпер деструктуризации и структуризации точки + добавлен менеджер редактирования фигур

---
+++ lib/esm/components/edit_manager.d.ts
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Bound } from '../helpers';
+interface EditManagerProps {
+    figureData: {
+        points?: Bound[];
+        radius?: number;
+        lat?: number;
+        lon?: number;
+        lng?: number;
+        southWest: Bound;
+        northEast: Bound;
+    };
+    figureType: number;
+    setData: React.Dispatch<React.SetStateAction<any>>;
+    destructPolyline?: boolean;
+    destructCircle?: boolean;
+    destructPolygon?: boolean;
+}
+export declare function EditManager(props: EditManagerProps): JSX.Element;
+export default EditManager;
