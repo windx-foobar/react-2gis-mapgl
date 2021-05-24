56a917fc4a4d184f8c6c32ecf88fe4b908f634cf
BarabanMotion <bezalkogoln1yCoder@gmail.com>
2021-05-24 16:00:43 +0300

    autogenerate

--- src/components/marker_html.tsx
+++ src/components/marker_html.tsx
@@ -58,7 +58,7 @@ export function HtmlMarker(props: HtmlMarkerOptions): null {
             marker.destroy();
          }
       }
-   }, [ map, props.coordinates[0], props.coordinates[1], props.html, props.handlers?.click ]);
+   }, [ map, props.coordinates[0], props.coordinates[1], props.html ]);
 
    return null;
 }
